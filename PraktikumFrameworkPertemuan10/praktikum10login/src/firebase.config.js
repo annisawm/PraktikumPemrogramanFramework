ad805b27fed86a3a05bbb95ddb6af86b9d8810e4
Annisa Wahyu Maulida <76188045+annisawm@users.noreply.github.com>
2022-05-17 21:41:09 +0700

    Pertemuan 10

---
+++ PraktikumFrameworkPertemuan10/praktikum10login/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyB_bFLbzHJ89zDDG-hvwqF3gLOPEMoOJV8",
+    authDomain: "pframework-311d8.firebaseapp.com",
+    projectId: "pframework-311d8",
+    storageBucket: "pframework-311d8.appspot.com",
+    messagingSenderId: "6300184946",
+    appId: "1:6300184946:web:eed8326c3e42115968edeb",
+    measurementId: "G-N11HYBL4PM"
+};
