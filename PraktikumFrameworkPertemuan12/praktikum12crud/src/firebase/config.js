1edcc51573ee060a3f43729c9c85b72031d59a28
Annisa Wahyu Maulida <76188045+annisawm@users.noreply.github.com>
2022-06-04 21:40:22 +0700

    Praktikum Minggu 12

---
+++ PraktikumFrameworkPertemuan12/praktikum12crud/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC3VbHUjes6WckJeF6pAOrGpKKlZehH9mY",
+    authDomain: "praktikum12-48d36.firebaseapp.com",
+    projectId: "praktikum12-48d36",
+    storageBucket: "praktikum12-48d36.appspot.com",
+    messagingSenderId: "405672869021",
+    appId: "1:405672869021:web:63eaee64aed94cc741a874",
+    measurementId: "G-9QPSSKKT8S"
+  };
+
+  export default firebaseConfig;
